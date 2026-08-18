568eaaf2d2e6c2b23ef600d0ebf9808b5dba696f
Alan K Biju <alan.k.biju25@gmail.com>
2026-08-18 16:53:17 +0530

    Keep signed-in users out of auth screens

---
+++ mobile/app/(auth)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Redirect, Stack } from "expo-router";
+import { useAuthStore } from "@/store/authStore";
+
+export default function AuthLayout() {
+  const user = useAuthStore((state) => state.user);
+  if (user) return <Redirect href="/(tabs)" />;
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
